21434be153c223923b8764fa4ce727bcf0a8e6ef
Roshan-Kumar-Microsoft <57892372+Roshan-Kumar-Microsoft@users.noreply.github.com>
2020-02-06 20:03:32 +0530

    terraform plan

---
+++ providers.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = "AKIAJU57KDE3FP7ZBS4A"
+  secret_key = "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"
+  region = "us-east-1"
+}
